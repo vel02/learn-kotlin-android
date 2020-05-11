e050c7382e2b7ed4902741c4ce98ce2b6ffa06ff
vel02 <arielaustria021@gmail.com>
2020-05-12 06:39:17 +0800

    Kotlin Course Calculator (done)

--- project-course/kotlin-android/Calculator/app/src/main/java/kiz/learnwithvel/calculator/MainActivity.kt
+++ project-course/kotlin-android/Calculator/app/src/main/java/kiz/learnwithvel/calculator/MainActivity.kt
@@ -1,5 +1,6 @@
 package kiz.learnwithvel.calculator
 
+//Using synthetic for binding views
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -13,15 +14,6 @@ private const val STATE_OPERAND_STORED: String = "STATE_OPERAND_STORED"
 
 class MainActivity : AppCompatActivity() {
 
-
-    //Approach #1: late initialization
-    //widgets
-//    private lateinit var mResult: EditText
-//    private lateinit var mNewNumber: EditText
-
-    //Approach #2: lazy initialization (thread safe as default) LazyThreadSafetyMode.NONE to of thread safe
-//    private val mDisplayOperation by lazy(LazyThreadSafetyMode.NONE) { findViewById<TextView>(R.id.tv_operation) }
-
     //variables to hold the operands and type of calculation
     private var mOperand1: Double? = null
     private var mPendingOperation = "="
@@ -30,34 +22,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        mResult = findViewById(R.id.edt_result)
-//        mNewNumber = findViewById(R.id.edt_newNumber)
-
-        // Data input buttons
-//        val btn0: Button = findViewById(R.id.btn_0)
-//        val btn1: Button = findViewById(R.id.btn_1)
-//        val btn2: Button = findViewById(R.id.btn_2)
-//        val btn3: Button = findViewById(R.id.btn_3)
-//        val btn4: Button = findViewById(R.id.btn_4)
-//        val btn5: Button = findViewById(R.id.btn_5)
-//        val btn6: Button = findViewById(R.id.btn_6)
-//        val btn7: Button = findViewById(R.id.btn_7)
-//        val btn8: Button = findViewById(R.id.btn_8)
-//        val btn9: Button = findViewById(R.id.btn_9)
-//        val btnDot: Button = findViewById(R.id.btn_dot)
-
-        // Operation buttons
-//        val btnEquals: Button = findViewById(R.id.btn_equal)
-//        val btnDivide: Button = findViewById(R.id.btn_divide)
-//        val btnMultiply: Button = findViewById(R.id.btn_multiply)
-//        val btnMinus: Button = findViewById(R.id.btn_minus)
-//        val btnPlus: Button = findViewById(R.id.btn_plus)
-
         // ============= Data Input Listener ============= //
 
         val mListener = View.OnClickListener { v ->
             val b = v as Button// up-casting
-            edt_newNumber.append(b.text)
+            if (b != btn_neg)
+                edt_newNumber.append(b.text)
+            else {
+                if (edt_newNumber.text.isEmpty()) {
+                    edt_newNumber.setText("-")
+                } else {
+                    var value = edt_newNumber.text.toString().toDouble()
+                    value *= -1
+                    edt_newNumber.setText(value.toString())
+                }
+            }
         }
 
         btn_0.setOnClickListener(mListener)
@@ -71,6 +50,7 @@ class MainActivity : AppCompatActivity() {
         btn_8.setOnClickListener(mListener)
         btn_9.setOnClickListener(mListener)
         btn_dot.setOnClickListener(mListener)
+        btn_neg.setOnClickListener(mListener)
 
         val mOpListener = View.OnClickListener { v ->
             val operation = (v as Button).text.toString()
@@ -85,6 +65,7 @@ class MainActivity : AppCompatActivity() {
 
             mPendingOperation = operation
             tv_operation.text = mPendingOperation
+            if (mPendingOperation == "=") mOperand1 = null
         }
 
         btn_equal.setOnClickListener(mOpListener)
@@ -99,8 +80,9 @@ class MainActivity : AppCompatActivity() {
         if (mOperand1 == null)
             mOperand1 = value
         else {
-            if (mPendingOperation == "=")
+            if (mPendingOperation == "=") {
                 mPendingOperation = operation
+            }
 
             when (mPendingOperation) {
                 "=" -> mOperand1 = value
@@ -115,7 +97,6 @@ class MainActivity : AppCompatActivity() {
 
         edt_result.setText(mOperand1.toString())
         edt_newNumber.setText("")
-
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -133,7 +114,7 @@ class MainActivity : AppCompatActivity() {
 
         mOperand1 = if (savedInstanceState.getBoolean(STATE_OPERAND_STORED, false)) {
             savedInstanceState.getDouble(STATE_OPERAND_VALUE)
-        } else null
+        } else 0.0
 
         tv_operation.text = mPendingOperation
         edt_result.setText(mOperand1.toString())
