2d84b72b11c8c0d12d718e1d1f3615de0096c2c4
vel02 <arielaustria021@gmail.com>
2020-05-13 05:36:56 +0800

    Kotlin Fundamental Class simplified constructor initialization

--- project-course/kotlin-fundamental/kotlin-oop/AccessModifiers/src/kiz/learnwithvel/accessmodifiers/Classes.kt
+++ project-course/kotlin-fundamental/kotlin-oop/AccessModifiers/src/kiz/learnwithvel/accessmodifiers/Classes.kt
@@ -10,14 +10,9 @@ fun main(args: Array<String>) {
  * Compare implementation to javacode to see the difference
  * of writing a class.
  * Using primary constructor.
+ *
+ * Simplified Implementation
  */
-class Employee constructor(firstName: String) {
+class Employee(val firstName: String) {
 
-    //property
-    val firstName: String
-
-    //initializer block
-    init {//connected to primary constructor
-        this.firstName = firstName
-    }
 }
