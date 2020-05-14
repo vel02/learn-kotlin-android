cb14f8e3def7edbe342edb9d3c131cd5b977f53e
Vel01 <arielaustria01@gmail.com>
2020-05-15 05:21:47 +0800

    Round to n decimal places way 2 uses ceiling (solved)

--- project-course/kotlin-problems/RoundNumber/src/kiz/learnwithvel/roundnumber/RoundNumber.kt
+++ project-course/kotlin-problems/RoundNumber/src/kiz/learnwithvel/roundnumber/RoundNumber.kt
@@ -1,11 +1,13 @@
 package kiz.learnwithvel.roundnumber
 
+import java.math.RoundingMode
 import java.text.DecimalFormat
 
 fun main(args: Array<String>) {
 
     val num = 1.34567
 
-    val decimalFormat = DecimalFormat("#,###.####")
+    val decimalFormat = DecimalFormat("#,###.###")
+    decimalFormat.roundingMode = RoundingMode.CEILING// rounding last decimal digit.
     println("Rounded: ${decimalFormat.format(num)}")
 }
