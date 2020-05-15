2113062d126ac503653791532bd7dbbd465dbf99
vel02 <arielaustria021@gmail.com>
2020-05-15 15:16:34 +0800

    Constant and data classes

--- project-course/kotlin-fundamental/kotlin-oop/AccessModifiers/src/kiz/learnwithvel/accessmodifiers/Classes.kt
+++ project-course/kotlin-fundamental/kotlin-oop/AccessModifiers/src/kiz/learnwithvel/accessmodifiers/Classes.kt
@@ -1,21 +1,51 @@
 package kiz.learnwithvel.accessmodifiers
 
+//constants
+const val MY_CONSTANT = 100
+
 fun main(args: Array<String>) {
-    val emp = Employee("John")
-    println(emp.firstName)
-    emp.fullTime = false
-    println(emp.fullTime)
+
+    println(MY_CONSTANT)
+
+    //using data class
+    val car = Car("Blue", "Toyota", 2015)
+    println(car)
+    val car2 = Car("Blue", "Toyota", 2015)
+    println(car == car2)
+    val car3 = car.copy()//reference copied
+    println(car3)
+    val car4 = car.copy(year = 2016)//copy except year
+    println(car4)
 
 
-    val emp2 = Employee("Joe")
-    println(emp2.firstName)
-    println(emp2.fullTime)
+    //using regular class
+    val emp = Employee("Ariel", false)
+    println(emp)
+    val emp2 = Employee("Ariel", false)
+    println(emp == emp2)//not implemented equals
 
-    val emp3 = Employee("Jane", false)
-    println(emp3.firstName)
-    println(emp3.fullTime)
+//    val emp = Employee("John")
+//    println(emp.firstName)
+//    emp.fullTime = false
+//    println(emp.fullTime)
+//
+//
+//    val emp2 = Employee("Joe")
+//    println(emp2.firstName)
+//    println(emp2.fullTime)
+//
+//    val emp3 = Employee("Jane", false)
+//    println(emp3.firstName)
+//    println(emp3.fullTime)
+//
+//    println(Demo().dummy)
+}
 
-    println(Demo().dummy)
+//data class or model
+data class Car(val color: String, val model: String, val year: Int) {
+    //get extra functionality, like toString(), a custom implementation of
+    //equals and hashcode, and a copy function
+    //can't be abstract, sealed or inner classes
 }
 
 /**
