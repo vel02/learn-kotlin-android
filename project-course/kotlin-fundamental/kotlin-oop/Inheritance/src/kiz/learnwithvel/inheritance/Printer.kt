43f17fcf4d72567cb329b0f67798bd1320f403f2
vel02 <arielaustria021@gmail.com>
2020-05-21 05:53:34 +0800

    inheritance with abstract, open, override keywords

--- project-course/kotlin-fundamental/kotlin-oop/Inheritance/src/kiz/learnwithvel/inheritance/Printer.kt
+++ project-course/kotlin-fundamental/kotlin-oop/Inheritance/src/kiz/learnwithvel/inheritance/Printer.kt
@@ -1,10 +1,21 @@
 package kiz.learnwithvel.inheritance
 
-open class Printer(val modelName : String) {
+
+fun main(args: Array<String>) {
+
+    val laserPrinter = LaserPrinter("Brother 1234")
+    laserPrinter.printModel()
+
+}
+
+abstract class Printer(val modelName: String) {
+
+    open fun printModel() = println("The model name of this printer is $modelName")
 
 }
 
 class LaserPrinter(modelName: String) : Printer(modelName) {
 
+    override fun printModel() = println("The model name of this laser printer is $modelName")
 
 }
