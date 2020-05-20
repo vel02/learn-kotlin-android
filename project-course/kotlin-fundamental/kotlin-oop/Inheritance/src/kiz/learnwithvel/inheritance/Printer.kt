61e1dee4b20c1dc6f88e5096cdda6c2792c56195
vel02 <arielaustria021@gmail.com>
2020-05-21 05:57:06 +0800

    inheritance with abstract methods overriding

--- project-course/kotlin-fundamental/kotlin-oop/Inheritance/src/kiz/learnwithvel/inheritance/Printer.kt
+++ project-course/kotlin-fundamental/kotlin-oop/Inheritance/src/kiz/learnwithvel/inheritance/Printer.kt
@@ -1,5 +1,6 @@
 package kiz.learnwithvel.inheritance
 
+//everything is public and final in kotlin (open key can allow you to override final methods
 
 fun main(args: Array<String>) {
 
@@ -12,10 +13,14 @@ abstract class Printer(val modelName: String) {
 
     open fun printModel() = println("The model name of this printer is $modelName")
 
+    abstract fun bestSellingPrice(): Double
+
 }
 
 class LaserPrinter(modelName: String) : Printer(modelName) {
 
     override fun printModel() = println("The model name of this laser printer is $modelName")
+
+    override fun bestSellingPrice() = 129.99
 
 }
