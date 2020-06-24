6fce28682e7455c1ace0109df7e9319489f83959
vel02 <arielaustria021@gmail.com>
2020-06-24 17:35:33 +0800

    kotlin smartherd: comments and variables + (initial commit)

---
+++ project-course/kotlin-smartherd/HelloWorld/src/kiz/learnwithvel/smartherd/HelloWorld.kt
@@ -0,0 +1,22 @@
+package kiz.learnwithvel.smartherd
+
+/**
+ * This is the main function. Entry point of the application
+ */
+fun main(args: Array<String>) {
+
+    var myNumber = 10
+    var myDecimal = 1.0
+
+    var myString: String // Mutable String
+    myString = "Hello, World"
+    myString = "Another World"
+
+    //Immutable String
+    val myAnotherStr = "My Constant String Value"
+
+
+    println("Hello, World")
+
+}
+
