6a29959bdf0b0eaa8c6fc0ecd290a6714406e1a6
Ariel Namias Austria <arielaustria021@gmail.com>
2020-06-26 17:46:47 +0800

    kotlin smartherd: JvmOverloads annotation to support interoperability for default functions

--- project-course/kotlin-smartherd/KotlinProgramming/src/interoperability/java/Main.java
+++ project-course/kotlin-smartherd/KotlinProgramming/src/interoperability/java/Main.java
@@ -9,6 +9,9 @@ public class Main {
         int sum = MyCustomKotlinFileName.add(3, 4);
         System.out.println("Printing sum from Java file: " + sum);
 
+        int result = MyCustomKotlinFileName.findVolume(4, 7);
+        System.out.println(result);
+
     }
 
     public static int getArea(int l, int b) {
--- project-course/kotlin-smartherd/KotlinProgramming/src/interoperability/kotlin/Main.kt
+++ project-course/kotlin-smartherd/KotlinProgramming/src/interoperability/kotlin/Main.kt
@@ -9,7 +9,13 @@ fun main(args: Array<String>) {
     var area = Main.getArea(10, 5)
     println("Printing area from Kotlin file: $area")
 
+    var result = findVolume(2, 3)
+    println(result)
+
 }
+
+@JvmOverloads
+fun findVolume(length: Int, breadth: Int, height: Int = 10) = length * breadth * height
 
 fun add(a: Int, b: Int): Int {
     return a + b
