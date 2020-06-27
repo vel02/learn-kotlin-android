bc6b904c9604904cf2f2093967a5d0a2bb57f0b9
Ariel Namias Austria <arielaustria021@gmail.com>
2020-06-27 16:18:53 +0800

    kotlin smartherd: secondary constructor part 2

--- project-course/kotlin-smartherd/KotlinProgramming/src/oop/Main.kt
+++ project-course/kotlin-smartherd/KotlinProgramming/src/oop/Main.kt
@@ -2,22 +2,29 @@ package oop
 
 fun main(args: Array<String>) {
 
-    var student = Student("Yel")
+    var student = Student("Yel", 20)
 
     println("Student has got a name as ${student.name}")
 
+    println(student.id)
 
 }
 
 //properties, and parameter
 class Student(var name: String) {
+
+    //initial value for id parameter, the property of secondary constructor
+    var id: Int = -1
+
+
     init {
         //this can be used as a primary constructor's body
-        println("Student age is $name")
+        println("Student age is $name and id is $id")
     }
 
     //secondary constructor
     constructor(name: String, id: Int) : this(name) {
-        //body
+        //the body of the secondary constructor is called after the init block
+        this.id = id
     }
 }
