239fbf5b0ac33194b24504d7aafeba995099a7cb
Ariel Namias Austria <arielaustria021@gmail.com>
2020-06-27 16:05:03 +0800

    kotlin smartherd: class and its properties and init block.

--- project-course/kotlin-smartherd/KotlinProgramming/src/oop/Main.kt
+++ project-course/kotlin-smartherd/KotlinProgramming/src/oop/Main.kt
@@ -1,2 +1,17 @@
 package oop
 
+fun main(args: Array<String>) {
+
+    var student = Student("Yel")
+
+    println("Student has got a name as ${student.name}")
+
+
+}
+
+class Student(var name: String) {
+
+    init {
+        println("Student has got a name as $name")
+    }
+}
