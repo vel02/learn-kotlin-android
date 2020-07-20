085e02848cfcc464a63a6ad3b723f1766ffe8788
vel02 <arielaustria021@gmail.com>
2020-07-20 08:57:03 +0800

    top10downloader: data download url path (done)

---
+++ project-course/kotlin-android/Top10Downloader-Application/Top10Downloader/app/src/main/java/kiz/learnwithvel/top10downloader/MainActivity.kt
@@ -0,0 +1,71 @@
+package kiz.learnwithvel.top10downloader
+
+import android.os.AsyncTask
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+
+        private class DownloadData : AsyncTask<String, Void, String>() {
+
+            override fun doInBackground(vararg url: String?): String {
+                Log.d(TAG, "doInBackground: starts with ${url[0]}")
+                val rssFeed = downloadXML(url[0])
+                if (rssFeed.isEmpty()) {
+                    Log.e(TAG, "doInBackground: Error downloading")
+                }
+                return rssFeed;
+            }
+
+            override fun onPostExecute(result: String?) {
+                Log.d(TAG, "onPostExecute: parameter is $result")
+            }
+
+            private fun downloadXML(urlPath: String?): String {
+//                //solution #1
+//                val xmlResult = StringBuilder()
+//
+//                try {
+//                    val url = URL(urlPath)
+//                    val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+//                    val code = connection.responseCode
+//                    Log.d(TAG, "downloadXML: code: $code")
+//
+//                    connection.inputStream
+//                        .buffered()
+//                        .reader()
+//                        .use { xmlResult.append(it.readText()) }
+//
+//                    return xmlResult.toString()
+//
+//                } catch (e: Exception) {
+//                    val message = when (e) {
+//                        is MalformedURLException -> "downloadXML: Invalid URL ${e.message}"
+//                        is IOException -> "downloadXML: IO Exception ${e.message}"
+//                        is SecurityException -> "downloadXML: SecurityException. Need permissions? ${e.message}"
+//                        else -> "downloadXML: Unknown ERROR: ${e.message}"
+//                    }
+//                    Log.e(TAG, "downloadXML: $message")
+//                }
+
+                //solution #2: with limit.
+                return URL(urlPath).readText()
+            }
+
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate() called.")
+        val downloadData = DownloadData()
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
+    }
+
+}
---
+++ project-course/kotlin-android/Top10Downloader-Application/prac-01/Top10Downloader/app/src/main/java/kiz/learnwithvel/top10downloader/MainActivity.kt
@@ -0,0 +1,76 @@
+package kiz.learnwithvel.top10downloader
+
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+
+        private const val TAG = "MainActivity"
+
+        private class DownloadData : AsyncTask<String, Void, String>() {
+
+            override fun doInBackground(vararg url: String?): String {
+                val rssFeed = downloadXML(url[0])
+                if (rssFeed.isEmpty()) {
+                    Log.e(TAG, "doInBackground: Error downloading.")
+                }
+                return rssFeed
+            }
+
+            override fun onPostExecute(result: String?) {
+                Log.d(TAG, "onPostExecute: result: $result")
+            }
+
+        }
+
+        private fun downloadXML(urlPath: String?): String {
+            val xmlResult = StringBuilder()
+
+            try {
+                val url = URL(urlPath)
+                val connection = url.openConnection() as HttpURLConnection
+                val code = connection.responseCode
+                Log.d(TAG, "downloadXML: connection: $code")
+
+                connection.inputStream
+                    .buffered()
+                    .reader()
+                    .use { xmlResult.append(it.readText()) }
+
+                return xmlResult.toString()
+            } catch (e: Exception) {
+                val message = when (e) {
+                    is MalformedURLException -> "Invalid URL ${e.message}"
+                    is IOException -> "IO Exception ${e.message}"
+                    is SecurityException -> "Security Exception ${e.message}"
+                    else -> "Unknown exception ${e.message}"
+                }
+                Log.e(TAG, "downloadXML: $message")
+            }
+
+            return ""
+        }
+
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val downloadData = DownloadData()
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
+    }
+
+}
