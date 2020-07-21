e933debd18e7159fee5cdcfc3f588951804ffa09
vel02 <arielaustria021@gmail.com>
2020-07-21 10:48:44 +0800

    top10downloader: optimized download

--- project-course/kotlin-android/Top10Downloader-Application/Top10Downloader/app/src/main/java/kiz/learnwithvel/top10downloader/MainActivity.kt
+++ project-course/kotlin-android/Top10Downloader-Application/Top10Downloader/app/src/main/java/kiz/learnwithvel/top10downloader/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import kiz.learnwithvel.top10downloader.adapter.FeedAdapter
@@ -33,6 +35,9 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val TAG = "MainActivity"
+        private const val KEY_LINK = "feed_link"
+        private const val KEY_LIMIT = "feed_limit"
+
 
         private class DownloadData(context: Context, list: ListView) :
             AsyncTask<String, Void, String>() {
@@ -47,7 +52,6 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun doInBackground(vararg url: String?): String {
-                Log.d(TAG, "doInBackground: starts with ${url[0]}")
                 val rssFeed = downloadXML(url[0])
                 if (rssFeed.isEmpty()) {
                     Log.e(TAG, "doInBackground: Error downloading")
@@ -75,18 +79,76 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private val downloadData by lazy { DownloadData(this, list_item) }
+    private var downloadData: DownloadData? = null
+    private var feedUrl: String =
+        "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=%d/xml"
+    private var feedLimit: Int = 10
+    private var feedCached = "CACHED_LINK"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "onCreate() called.")
-        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
+
+        if (savedInstanceState != null) {
+            feedUrl = savedInstanceState.getString(KEY_LINK)!!
+            feedLimit = savedInstanceState.getInt(KEY_LIMIT)
+        }
+        downloadUrl(feedUrl.format(feedLimit))
+    }
+
+    private fun downloadUrl(url: String) {
+        if (url != feedCached) {
+            Log.d(TAG, "downloadUrl: called")
+            downloadData = DownloadData(this, list_item)
+            downloadData?.execute(url)
+            feedCached = url
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_feed, menu)
+        if (feedLimit == 10) {
+            menu?.findItem(R.id.action_top10)?.isChecked = true
+        } else menu?.findItem(R.id.action_top25)?.isChecked = true
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_free ->
+                feedUrl =
+                    "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=%d/xml"
+            R.id.action_paid ->
+                feedUrl =
+                    "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=%d/xml"
+            R.id.action_songs ->
+                feedUrl =
+                    "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=%d/xml"
+            R.id.action_top10, R.id.action_top25 -> {
+                if (!item.isChecked) {
+                    item.isChecked = true
+                    feedLimit = 35 - feedLimit
+                }
+            }
+            R.id.action_refresh -> {
+                feedCached = "CACHED_LINK"
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+        downloadUrl(feedUrl.format(feedLimit))
+        return true
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(KEY_LINK, feedUrl)
+        outState.putInt(KEY_LIMIT, feedLimit)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        downloadData.cancel(true)
+        downloadData?.cancel(true)
     }
 
 }
