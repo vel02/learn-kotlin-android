0cf10454a87890ab72836964a3d7c3da6c600d93
vel02 <arielaustria021@gmail.com>
2020-08-01 11:44:35 +0800

    task-timer #1.0: initial commit

---
+++ project-course/kotlin-android/TaskTimer/app/src/main/java/kiz/learnwithvel/tasktimer/MainActivityFragment.kt
@@ -0,0 +1,20 @@
+package kiz.learnwithvel.tasktimer
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class MainActivityFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
