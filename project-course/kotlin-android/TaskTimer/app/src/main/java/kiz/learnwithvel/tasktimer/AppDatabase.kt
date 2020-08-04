e545a459a6bb0cfdc7e2e9d8d1689d822e0d2d86
vel02 <arielaustria021@gmail.com>
2020-08-04 11:36:04 +0800

    task-timer #4.0: AppDatabase.kt preparation

---
+++ project-course/kotlin-android/TaskTimer/app/src/main/java/kiz/learnwithvel/tasktimer/AppDatabase.kt
@@ -0,0 +1,40 @@
+package kiz.learnwithvel.tasktimer
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+
+/**
+ * Basic database class for the application.
+ *
+ * The only class that should use this is [AppProvider].
+ */
+
+private const val TAG = "AppDatabase"
+
+private const val DATABASE_NAME = "TaskTimer.db"
+private const val DATABASE_VERSION = 1
+
+internal class AppDatabase(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    init {
+        Log.d(TAG, "AppDatabase: initialising ")
+    }
+
+    override fun onCreate(database: SQLiteDatabase) {
+        Log.d(TAG, "onCreate: starts")
+        val sSQL = """CREATE TABLE ${TasksContract.TABLE_NAME} (
+            ${TasksContract.Columns.ID} INTEGER PRIMARY KEY NOT NULL,
+            ${TasksContract.Columns.TASK_NAME} TEXT NOT NULL,
+            ${TasksContract.Columns.TASK_DESCRIPTION} TEXT,
+            ${TasksContract.Columns.TASK_SORT_ORDER} INTEGER);""".replaceIndent(" ")
+        Log.d(TAG, sSQL)
+        database.execSQL(sSQL)
+    }
+
+    override fun onUpgrade(database: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+
+    }
+}
